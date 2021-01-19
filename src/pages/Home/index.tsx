19aea46d0dfdc057a05a6161dce8568c4e53927c
Luiz Morais <luiz.ado@hotmail.com>
2021-01-19 09:48:21 -0300

    Created FoodDetail Folder. Removed some unnecessary methods from Home page.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import CustomText from '../../components/CustomText';
 import FoodScrollView from '../../components/FoodScrollView';
